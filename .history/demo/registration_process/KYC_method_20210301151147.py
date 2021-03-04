67a118268896807345e99535b559fcc994537830
Tyler96-QA <1718459369@qq.com>
2021-03-04 11:36:58 +0800

    待完善注册及KYC成功后的验证/0304

---
+++ .history/demo/registration_process/KYC_method_20210301151147.py
@@ -0,0 +1,122 @@
+import time
+import os
+import sys
+"""跨目录调用，需要将导入的包加入sys.path中"""
+path=os.path.dirname(os.path.dirname(__file__))
+path=os.path.join(path,'publick')
+sys.path.append(path)
+from browser_actions import Commonweb
+from other_actions import public_method
+
+#实例化对象
+pub_method=public_method()
+
+#继承基本类
+class kyc_approve(Commonweb):
+    global driver
+
+    #默认以谷歌浏览器执行测试用例
+    def browsertype(self,browsername='Chrome'):
+        self.driver=self.open_browser(browsername)
+
+    #去除登录页弹窗
+    def login_topup(self):
+        try:
+            self.web_click('css,.blk-sure-btn')
+        except Exception as msg:
+            pub_method.log_output('!!--!!topup').error('页面弹窗去除失败：{}'.format(msg))
+
+    #选择会员中心登录页语言
+    def cp_lang(self,lang):
+        try:
+            self.web_click('css,.el-icon-arrow-down')
+            time.sleep(1)
+            if lang=='EN' or lang=='英语':
+                self.web_click('css,.el-dropdown-menu__item')
+            elif lang=='CN' or lang=='简中':
+                self.web_click('css,.el-dropdown-menu__item',1)
+        except Exception as msg:
+            pub_method.log_output('!!--!!lang').error('cp页面语言选择错误,参数CN/EN：{}'.format(msg))
+
+    #选择bos登录页语言
+    def bos_lang(self,lang):
+        try:
+            if lang=='CN' or lang=='简中':
+                self.web_click('css,.ivu-icon-ios-arrow-down')
+                time.sleep(1)
+                self.web_click('css,.ivu-select-item') #选择页面语言为中文
+            else:
+                pass
+        except Exception as msg:
+            pub_method.log_output('!!--!!lang').error('bos页面语言选择错误,参数CN/EN：{}'.format(msg)) 
+
+    #访问会员中心及BOS登录页,选择页面语言
+    def loginweb(self,lang):
+        try:
+            self.open_web('https://at-client-portal-sit.atfxdev.com/login')
+            #点击弹窗
+            self.login_topup()
+            time.sleep(1)
+            #选择页面语言
+            self.cp_lang(lang)
+            time.sleep(1)
+            #新开窗口访问bos登录页
+            self.js_openwindows('https://at-bos-frontend-sit.atfxdev.com/login')
+            #切换窗口
+            self.switch_windows(1)
+            #选择页面语言
+            self.bos_lang(lang)
+            time.sleep(1)
+        except Exception as msg:
+            pub_method.log_output('!!--!!loginweb').error('访问cp/bos登录页失败：{}'.format(msg))
+   
+    #登录会员中心
+    def login_cp(self,username,psword):
+        try:
+            #切换窗口
+            self.switch_windows(0)
+            #输入用户名
+            self.web_input('css,.el-input__inner',username,1)
+            time.sleep(1)
+            #输入密码
+            self.web_input('css,.el-input__inner',psword,3)
+            time.sleep(1)
+            #点击登录
+            self.web_click('css,.login-btn')
+            time.sleep(3)
+        except Exception as msg:
+            pub_method.log_output('!!--!!lgoin_cp').error('登录会员中心失败：{}'.format(msg))
+
+    #登出会员中心
+    def logout_cp(self):
+        try:
+            self.web_click('css,.el-icon--right')
+            time.sleep(1)
+            self.web_click('css,.drop-sub-title',12)
+        except Exception as msg:
+            pub_method.log_output('!!--!!lgoout_cp').error('登出会员中心失败：{}'.format(msg))
+
+    #登录bos
+    def login_bos(self,username,psword):
+        try:
+            #输入bos用户名
+            self.web_input('css,.ivu-input-default',username)
+            time.sleep(1)
+            #输入密码
+            self.web_input('css,.ivu-input-default',psword,1)
+            time.sleep(1)
+            #点击登录
+            self.web_click('css,.ivu-btn-large')
+            time.sleep(1)
+        except Exception as msg:
+            pub_method.log_output('!!--!!lgoin-bos').error('登录bos失败：{}'.format(msg))
+
+    #KYC认证操作
+    def get_onkyc(self):
+        #认证联系方式
+        self.web_click('css,.el-button--primary')
+        time.sleep(1)
+        
+
+            
+
---
+++ .history/demo/registration_process/kyc_operating_20210302172331.py
@@ -0,0 +1,56 @@
+from KYC_method import kyc_approve
+from selenium import webdriver
+import unittest
+import ddt
+import os
+import sys
+path=os.path.dirname(os.path.dirname(__file__))
+path=os.path.join(path,'publick')
+sys.path.append(path)
+from about_data import exceldata
+
+#实例化对象
+kyc=kyc_approve()
+e=exceldata()
+rows=e.openexcel(r'E:\test\account_number.xlsx','Sheet1') #测试文档的路径，sheet名,并获取总行数
+testdata=e.dict_data()
+
+@ddt.ddt
+class kyc_actions(unittest.TestCase):
+
+    @classmethod 
+    def setUpClass(cls):
+        kyc.browsertype() #默认以谷歌浏览器打开
+        #访问CP及BOS登录页，选择页面语言
+        kyc.loginweb('CN')
+        kyc.login_bos('tyler.tang','Tl123456')
+        #打开客户名单页
+        kyc.open_customerlist_()
+    
+    def tearDown(self):
+        #执行最后一个测试用例后关闭浏览器
+        if self.data_index==testdata.index(testdata[-1]):
+            kyc.quitdriver()
+            print(11111)
+        else:
+            #登出会员中心
+            kyc.logout_cp()
+            #清空主账号搜索条件
+            kyc.clearaccount()
+
+
+    @ddt.data(*testdata)
+    def test_kyc(self,data):
+        #获取每组测试数据的下标
+        self.data_index=testdata.index(data)
+        #除了第一个测试用例外，其他用例都必须点击登录页弹窗
+        if self.data_index!=0:
+            kyc.login_topup()
+        #登录会员中心
+        kyc.login_cp(data['邮箱'],'Tl123456')
+        kyc.get_on_kyc(data['主账户'])
+       
+
+
+if __name__=='__main__':
+    unittest.main()
