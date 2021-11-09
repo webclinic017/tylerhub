c7f9a45dc25e0522c38d9d6bffb8f0109d8b03d7
Tyler96-QA <1718459369@qq.com>
2021-11-09 11:07:53 +0800

    补全出入金列表

---
+++ demo/deposit_withdrawal_list/location/location_of_deposti_withdrawal_List.py
@@ -0,0 +1,8 @@
+'''
+Author: your name
+Date: 2021-11-09 11:05:58
+LastEditTime: 2021-11-09 11:05:58
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \tylerhub\demo\deposit_withdrawal_list\location\location_of_deposti_withdrawal_List.py
+'''
