6a447480812939071a9cb1033a2f49f6c9000162
Tyler Tang <tyler.tang@6317.io>
2022-06-02 11:25:13 +0800

    删除多余出入金列表脚本0602

--- demo/deposit_withdrawal_list/location/location_of_deposti_withdrawal_List.py
+++ demo/deposit_withdrawal_list/location/location_of_deposti_withdrawal_List.py
@@ -1,7 +1,7 @@
 '''
 Author: tyler
 Date: 2021-09-17 15:00:40
-LastEditTime: 2022-05-31 11:31:24
+LastEditTime: 2022-06-02 11:19:24
 LastEditors: Tyler Tang tyler.tang@6317.io
 Description: Page operation
 FilePath: \tylerhub\demo\walaopay_withdrawal\location\location_of_walaopay_withdrawal.py
@@ -47,7 +47,7 @@ class Location_of_deposit_withdrawal(object):
         self.comMethod=Commonmethod()
 
 
-        #登录页
+    #登录页
     def get_url(self,environment,username,password,lang='CN'):
         try:
             common.open_web(conFig.get_value('bos_login', '{}'.format(environment)))
@@ -78,9 +78,22 @@ class Location_of_deposit_withdrawal(object):
         time.sleep(0.5)
         #登录
         common.display_click('css,.ivu-btn > span',-1)
-        time.sleep(3)
-        #出入金记录页面
+        time.sleep(1)
         common.switch_windows(1)
+        #判断页面是否加载完成
+        while True:
+            if common.ele_is_displayed("css,[src='/static/img/loading.webm']",1):
+                continue
+            else:
+                break
+        time.sleep(1)
+        while True:
+            self.loading_attributes=common.get_attributes('css,.el-loading-mask', 'style')
+            if 'none' in self.loading_attributes:
+                break
+            else:
+                continue
+        #出入金记录页面
         time.sleep(1)
         common.display_click("xpath,//span[.='出入金记录']")
         time.sleep(2)
@@ -88,17 +101,21 @@ class Location_of_deposit_withdrawal(object):
         common.display_click('css,.el-select .el-select__caret',-1)
         time.sleep(0.5)
         common.display_click("xpath,//div[@class='formAndList']//span[.='全部']",-1)
-        time.sleep(2)
+        time.sleep(1)
 
 
     #筛选时间查询
     def serch_list(self,account):
         try:
+            #筛选类型为所有
+            common.display_click('css,.el-select .el-select__caret',2)
+            time.sleep(0.5)
+            common.display_click("xpath,//span[.='类型(全部)']")
             time.sleep(1)
             common.display_click('css,.el-select .el-select__caret',1)
             time.sleep(1)
-            #随机选择时间random.randint(1, 6)
-            self.index=random.randint(1, 6)
+            #随机选择时间random.randint(0, 6)
+            self.index=random.randint(0, 6)
             #今天
             self.nowTime=datetime.datetime.now()
             #本月第一天
@@ -107,7 +124,12 @@ class Location_of_deposit_withdrawal(object):
             self.lastmonEnd=self.monStart-datetime.timedelta(days=1)
             time.sleep(1)
             common.display_click("css,[x-placement='bottom-start'] li",self.index)
-            time.sleep(3)
+            time.sleep(1)
+            while True:
+                if 'none' not in common.get_attributes('css,.el-loading-mask', 'style'):
+                    continue
+                else:
+                    break
             if self.index==1:
                 #今天
                 self.dateStart=self.nowTime.strftime('%Y-%m-%d')
@@ -128,10 +150,14 @@ class Location_of_deposit_withdrawal(object):
                 #本月
                 self.dateStart=self.monStart.strftime('%Y-%m-%d')
                 self.dateEnd=self.nowTime.strftime('%Y-%m-%d')
-            else:
+            elif self.index==6:
                 #上月
                 self.dateStart=(datetime.datetime(self.lastmonEnd.year, self.lastmonEnd.month, 1)).strftime('%Y-%m-%d')
                 self.dateEnd=self.lastmonEnd.strftime('%Y-%m-%d')
+            else:
+                #全部
+                self.dateStart='1990-01-01'
+                self.dateEnd='2055-12-31' 
             #转换mongodb数据库查询时间格式
             self.dateGte=parser.parse('{}T00:00:00Z'.format(self.dateStart))
             self.dateLte=parser.parse('{}T23:59:59Z'.format(self.dateEnd))
@@ -143,11 +169,11 @@ class Location_of_deposit_withdrawal(object):
                 #出金表
                 self.mongodbWithdrawal=dataBase.search_in_mongodb(conFig.get_value('mongodb', 'uri'),
                 'atfxgm-sit', 'atfx_withdrawal',{"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},
-                {"$or":[{"currStatus":'S'},{"currStatus":'U'}]},{"accountNumber": account}]},N=0)
+                {"$or":[{"currStatus":'S'},{"currStatus":'U'},{"currStatus":'L'}]},{"accountNumber": account}]},N=0)
                 #入金表
                 self.mongodbDeposit=dataBase.search_in_mongodb(conFig.get_value('mongodb', 'uri'),
                 'atfxgm-sit', 'atfx_deposit',{"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},
-                {"$or":[{"currStatus":'S'},{"currStatus":'U'}]},{"accountNumber": account}]},N=0)
+                {"$or":[{"currStatus":'S'},{"currStatus":'U'},{"currStatus":'L'}]},{"accountNumber": account}]},N=0)
                 #转账表
                 self.mongodbTransfer=dataBase.search_in_mongodb(conFig.get_value('mongodb', 'uri'),'atfxgm-sit', 'atfx_fund_transfer',
                 {"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},{"$or":[{"currStatus":'S'},{"currStatus":'U'}]},
@@ -162,35 +188,47 @@ class Location_of_deposit_withdrawal(object):
     
     #时间段，交易金额，汇率，MT金额，时间，类型，管道，状态和Mongo数据进行核对。
     def search_withdrawal(self,account):
-        #查询数据库,默认查询状态为成功的订单
+        #查询数据库,默认查询订单
+        
+        #类型选择出金
+        common.display_click('css,.el-select .el-select__caret',2)
+        time.sleep(0.5)
+        common.display_click("xpath,//body[@id='bodyBgImg']/div[@class='el-select-dropdown el-popper']//span[.='出金']")
+        time.sleep(0.5)
+        
+        common.display_click("xpath,//i[@class='el-icon-search']")
+        time.sleep(1)
+        while True:
+            if 'none' not in common.get_attributes('css,.el-loading-mask', 'style'):
+                continue
+            else:
+                break
         #出金记录
-        if common.ele_is_displayed("xpath,//tbody//span[.='出金']",2):
+        if common.ele_is_displayed("xpath,//tbody//span[.='出金']", 1):
             self.withdrawal_len=common.get_lenofelement("xpath,//tbody//span[.='出金']")
             print('页面出金记录{}条'.format(self.withdrawal_len))
             #查询数据库
             self.mongodbWithdrawal=dataBase.search_in_mongodb(conFig.get_value('mongodb', 'uri'),'atfxgm-sit', 'atfx_withdrawal',
-            {"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},{"$or":[{"currStatus":'S'},{"currStatus":'U'}]},{"accountNumber": account}]},
+            {"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},{"$or":[{"currStatus":'S'},{"currStatus":'U'},{"currStatus":'L'}]},{"accountNumber": account}]},
             'createDate_mt','settleAmt','realRate','mtAmt','currStatus','channel',N=0)
 
 
             #处理页面数据
             self.withdrawal_list=[]
-            for i in range(0,self.list_len):
-                if common.get_text('css,tbody > tr > .el-table_1_column_7 div span',i)=='出金':
-                    self.withdrawal_dict={}
+            for i in range(0,self.withdrawal_len):
+                self.withdrawal_dict={}
 
-                    self.timestr=common.get_text('css,tbody > tr > .el-table_1_column_6 div span',i)
+                self.timestr=common.get_text('css,tbody > tr > .el-table_1_column_6 div span',i)
 
-                    self.withdrawal_dict['createDate_mt']=datetime.datetime.strptime(self.timestr,'%d/%m/%Y %H:%M')
-                    self.withdrawal_dict['settleAmt']=float(randomData.extract_numbers
-                    (common.get_text('css,tbody > tr > .el-table_1_column_3 div span',i)))/100
-                    self.withdrawal_dict['realRate']=float(common.get_text('css,tbody > tr > .el-table_1_column_4 div span',i))
-                    self.withdrawal_dict['mtAmt']=float(randomData.extract_numbers
-                    (common.get_text('css,tbody > tr > .el-table_1_column_5 div span',i)))/100
-                    self.withdrawal_dict['currStatus']=common.get_text('css,tbody > tr > .el-table_1_column_9 div span:nth-of-type(2)',i)
-                    self.withdrawal_list.append(self.withdrawal_dict)
-                else:
-                    pass
+                self.withdrawal_dict['createDate_mt']=datetime.datetime.strptime(self.timestr,'%d/%m/%Y %H:%M')
+                self.withdrawal_dict['settleAmt']=float(randomData.extract_numbers
+                (common.get_text('css,tbody > tr > .el-table_1_column_3 div span',i)))/100
+                self.withdrawal_dict['realRate']=float(common.get_text('css,tbody > tr > .el-table_1_column_4 div span',i))
+                self.withdrawal_dict['mtAmt']=float(randomData.extract_numbers
+                (common.get_text('css,tbody > tr > .el-table_1_column_5 div span',i)))/100
+                self.withdrawal_dict['currStatus']=common.get_text('css,tbody > tr > .el-table_1_column_9 div span:nth-of-type(2)',i)
+                self.withdrawal_list.append(self.withdrawal_dict)
+
             return True
         else:
             self.withdrawal_len=0
@@ -198,35 +236,47 @@ class Location_of_deposit_withdrawal(object):
             #查询数据库
             self.mongodbWithdrawal=dataBase.search_in_mongodb(conFig.get_value('mongodb', 'uri'),
             'atfxgm-sit', 'atfx_withdrawal',{"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},
-            {"$or":[{"currStatus":'S'},{"currStatus":'U'}]},{"accountNumber": account}]},N=0)
+            {"$or":[{"currStatus":'S'},{"currStatus":'U'},{"currStatus":'L'}]},{"accountNumber": account}]},N=0)
             return False
     
     def search_deposit(self,account):
+        #类型选择入金
+        common.display_click('css,.el-select .el-select__caret',2)
+        time.sleep(0.5)
+        common.display_click("xpath,//body[@id='bodyBgImg']/div[@class='el-select-dropdown el-popper']//span[.='入金']")
+        time.sleep(0.5)
+        
+        #搜索
+        common.display_click("xpath,//i[@class='el-icon-search']")
+        time.sleep(1)
+        while True:
+            if 'none' not in common.get_attributes('css,.el-loading-mask', 'style'):
+                continue
+            else:
+                break
         #入金记录
-        if common.ele_is_displayed("xpath,//tbody//span[.='入金']",2):
+        if common.ele_is_displayed("xpath,//tbody//span[.='入金']",1):
             self.deposit_len=common.get_lenofelement("xpath,//tbody//span[.='入金']")
             print('页面入金记录{}条'.format(self.deposit_len))
             #查询数据库
             self.mongodbDeposit=dataBase.search_in_mongodb(conFig.get_value('mongodb', 'uri'),'atfxgm-sit', 'atfx_deposit',
-            {"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},{"$or":[{"currStatus":'S'},{"currStatus":'U'}]},{"accountNumber": account}]},
+            {"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},{"$or":[{"currStatus":'S'},{"currStatus":'U'},{"currStatus":'L'}]},{"accountNumber": account}]},
             'createDate_mt','fromAmt','rate','mt4Amt','currStatus','channel',N=0)
 
             #处理页面数据
             self.deposti_list=[]
-            for i in range(0,self.list_len):
-                if common.get_text('css,tbody > tr > .el-table_1_column_7 div span',i)=='入金':
-                    self.deposit_dict={}
-                    self.timestr=common.get_text('css,tbody > tr > .el-table_1_column_6 div span',i)
-                    self.deposit_dict['createDate_mt']=datetime.datetime.strptime(self.timestr,'%d/%m/%Y %H:%M')
-                    self.deposit_dict['fromAmt']=int(randomData.extract_numbers
-                    (common.get_text('css,tbody > tr > .el-table_1_column_3 div span',i)))/100
-                    self.deposit_dict['rate']=float(common.get_text('css,tbody > tr > .el-table_1_column_4 div span',i))
-                    self.deposit_dict['mt4Amt']=float(randomData.extract_numbers
-                    (common.get_text('css,tbody > tr > .el-table_1_column_5 div span',i)))/100
-                    self.deposit_dict['currStatus']=common.get_text('css,tbody > tr > .el-table_1_column_9 div span:nth-of-type(2)',i)
-                    self.deposti_list.append(self.deposit_dict)  
-                else:
-                    pass
+            for i in range(0,self.deposit_len):
+        
+                self.deposit_dict={}
+                self.timestr=common.get_text('css,tbody > tr > .el-table_1_column_6 div span',i)
+                self.deposit_dict['createDate_mt']=datetime.datetime.strptime(self.timestr,'%d/%m/%Y %H:%M')
+                self.deposit_dict['fromAmt']=int(randomData.extract_numbers
+                (common.get_text('css,tbody > tr > .el-table_1_column_3 div span',i)))/100
+                self.deposit_dict['rate']=float(common.get_text('css,tbody > tr > .el-table_1_column_4 div span',i))
+                self.deposit_dict['mt4Amt']=float(randomData.extract_numbers
+                (common.get_text('css,tbody > tr > .el-table_1_column_5 div span',i)))/100
+                self.deposit_dict['currStatus']=common.get_text('css,tbody > tr > .el-table_1_column_9 div span:nth-of-type(2)',i)
+                self.deposti_list.append(self.deposit_dict)  
             return True
         else:
             #查询数据库
@@ -234,11 +284,25 @@ class Location_of_deposit_withdrawal(object):
             print('页面入金记录{}条'.format(self.deposit_len))
             self.mongodbDeposit=dataBase.search_in_mongodb(conFig.get_value('mongodb', 'uri'),
             'atfxgm-sit', 'atfx_deposit',{"$and": [{"createDate_mt": {"$gte": self.dateGte,"$lte": self.dateLte}},
-            {"$or":[{"currStatus":'S'},{"currStatus":'U'}]},{"accountNumber": account}]},N=0)
+            {"$or":[{"currStatus":'S'},{"currStatus":'U'},{"currStatus":'L'}]},{"accountNumber": account}]},N=0)
             return False
 
     
     def search_transfer(self,account):
+        #类型选择转账
+        common.display_click('css,.el-select .el-select__caret',2)
+        time.sleep(0.5)
+        common.display_click("xpath,//body[@id='bodyBgImg']/div[@class='el-select-dropdown el-popper']//span[.='转账']")
+        time.sleep(0.5)
+
+        #搜索
+        common.display_click("xpath,//i[@class='el-icon-search']")
+        time.sleep(1)
+        while True:
+            if 'none' not in common.get_attributes('css,.el-loading-mask', 'style'):
+                continue
+            else:
+                break
         #转账记录
         if common.ele_is_displayed("xpath,//tbody//span[.='转账']",2):
             self.transfer_len=common.get_lenofelement("xpath,//tbody//span[.='转账']")
@@ -250,17 +314,15 @@ class Location_of_deposit_withdrawal(object):
 
             #处理页面数据
             self.transfer_list=[]
-            for i in range(0,self.list_len):
-                if common.get_text('css,tbody > tr > .el-table_1_column_7 div span',i)=='转账':
-                    self.transfer_dict={}
-                    self.timestr=common.get_text('css,tbody > tr > .el-table_1_column_6 div span',i)
-                    self.transfer_dict['createDate_mt']=datetime.datetime.strptime(self.timestr,'%d/%m/%Y %H:%M')
-                    self.transfer_dict['toMtAmt']=float(randomData.extract_numbers
-                    (common.get_text('css,tbody > tr > .el-table_1_column_5 div span',i)))/100
-                    self.transfer_dict['currStatus']=common.get_text('css,tbody > tr > .el-table_1_column_9 div span:nth-of-type(2)',i)
-                    self.transfer_list.append(self.transfer_dict)
-                else:
-                    pass
+            for i in range(0,self.transfer_len):
+                self.transfer_dict={}
+                self.timestr=common.get_text('css,tbody > tr > .el-table_1_column_6 div span',i)
+                self.transfer_dict['createDate_mt']=datetime.datetime.strptime(self.timestr,'%d/%m/%Y %H:%M')
+                self.transfer_dict['toMtAmt']=float(randomData.extract_numbers
+                (common.get_text('css,tbody > tr > .el-table_1_column_5 div span',i)))/100
+                self.transfer_dict['currStatus']=common.get_text('css,tbody > tr > .el-table_1_column_9 div span:nth-of-type(2)',i)
+                self.transfer_list.append(self.transfer_dict)
+
             print('当前时间段 {} 至 {} 出入金记录为{}条：出金{}条，入金{}条，转账{}条'.format(
             self.dateStart,self.dateEnd,self.list_len,self.withdrawal_len,self.deposit_len,self.transfer_len))
             return True
